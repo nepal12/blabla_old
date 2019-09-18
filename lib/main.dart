3859ddbe0d3195f5a237ffb5cc0787a005af7160
nepal12 <riteshrijal@yahoo.com>
2019-09-18 18:08:17 +0200

    update 6 to 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'pages/ConversationPageList.dart';
+import 'config/Palette.dart';
+import 'pages/ConversationPageSlide.dart';
 
 void main() => runApp(Blabla());
 
@@ -8,11 +9,11 @@ class Blabla extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Blabla',
+      title: 'Blabla..',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Palette.primaryColor,
       ),
-      home: ConversationPageList(),
+      home: ConversationPageSlide(),
     );
   }
 }
--- lib/pages/ConversationPage.dart
+++ lib/pages/ConversationPage.dart
@@ -1,37 +1,44 @@
-/**
- * Conversation Page
- */
 import 'package:flutter/material.dart';
+import 'package:blabla/config/Palette.dart';
+import 'package:blabla/pages/ConversationBottomSheet.dart';
 import 'package:blabla/widgets/ChatAppBar.dart';
 import 'package:blabla/widgets/ChatListWidget.dart';
 import 'package:blabla/widgets/InputWidget.dart';
 
 class ConversationPage extends StatefulWidget {
-
   @override
   _ConversationPageState createState() => _ConversationPageState();
+
   const ConversationPage();
 }
 
 class _ConversationPageState extends State<ConversationPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+            key: _scaffoldKey,
             appBar: ChatAppBar(), // Custom app bar for chat screen
-            body: Stack(children: <Widget>[
-              Column(
-                children: <Widget>[
-                  ChatListWidget(),
-                  InputWidget()
-                ],
-              ),
-            ]
-            )
-        )
-    );
+            body: Container(
+                color: Palette.chatBackgroundColor,
+                child:Stack(children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      ChatListWidget(),
+                      GestureDetector(
+                          child: InputWidget(),
+                          onPanUpdate: (details) {
+                            if (details.delta.dy <0) {
+                              _scaffoldKey.currentState
+                                  .showBottomSheet<Null>((BuildContext context) {
+                                return ConversationBottomSheet();
+                              });
+                            }
+                          })
+                    ],
+                  ),
+                ]))));
   }
-
-
 }
